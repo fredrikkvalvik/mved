047e068ff27aa0783a1d06776e5e19575f2df48b
Fredrik Kvalvik <fredrik.kvalvik@gmail.com>
2026-07-03 11:53:59 +0200

    deps: add cobra

--- main.go
+++ main.go
@@ -1,3 +1,9 @@
+// vfs is a visual file system manager. The idea is to be able to
+//   - move files
+//   - rename files
+//   - delete files
+//
+// All of this is done using $EDTIOR.
 package main
 
 import "fmt"
